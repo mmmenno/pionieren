92020c2aaec26a27c7b7bd2d4a37979f250736a4
Menno den Engelse <menno@denengelse.nl>
2022-01-24 08:19:04 +0100

    ff testen

--- index.php
+++ index.php
@@ -64,6 +64,9 @@ $context = stream_context_create($opts);
 // Open the file using the HTTP headers set above
 $json = file_get_contents($url, false, $context);
 
+echo $json;
+die;
+
 $data = json_decode($json,true);
 
 $all = $data['results']['bindings'];
