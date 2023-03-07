33791265f244358acf9db6f78c5d7f79bda8fede
Menno den Engelse <menno@denengelse.nl>
2023-03-07 09:41:44 +0100

    debug

--- straat.php
+++ straat.php
@@ -94,6 +94,8 @@ $context = stream_context_create($opts);
 // Open the file using the HTTP headers set above
 $json = file_get_contents($url, false, $context);
 
+echo $json;
+
 $data = json_decode($json,true);
 
 $firstrow = $data['results']['bindings'][0];
